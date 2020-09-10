0b935b201327819dc4c27144cc490ad110a4fd44
leyner <testbejarano@gmail.com>
2020-09-10 03:07:12 -0500

    minima parte grafica que le meti al asunto ya funciona junto con la logica de la regla falsa

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
 import PySimpleGUI as sg      
+from regla_falsa import *
 
 sg.theme('DarkAmber')    # Keep things interesting for your users
 
-layout = [[sg.Text('Persistent window')],
-          [sg.Text("-- dada la funcion  math.pow(x,3) + 2*math.pow(x,2) - 3*x - 1")],
+layout = [[sg.Text("-- dada la funcion  math.pow(x,3) + 2*math.pow(x,2) - 3*x - 1")],
           [sg.Text("-- y basado en el siguiente input de ejemplo")],
-          [sg.Text("-- Input de ejemplo: regulaFalsi(1, 2, 0.0004, 100)")],      
-          [sg.Input(key='-IN-')],      
+          [sg.Text("-- Input de ejemplo: regulaFalsi(1, 2, 0.0004, 100)")], 
+          [sg.Text("indique el punto inferior, parametro poscisional 1")],      
+          [sg.Input(key='a_Param')], 
+          [sg.Text("indique el punto superior, parametro poscisional 2")],      
+          [sg.Input(key='b_Param')], 
+          [sg.Text("indique la tolerancia, parametro poscisional 3")],      
+          [sg.Input(key='tol_Param')],
+          [sg.Text("indique el numero maximo de iteraciones, parametro poscisional 4")],      
+          [sg.Input(key='n_Param')],
           [sg.Button('Read'), sg.Exit()]]      
 
 window = sg.Window('Regla Falsa', layout)      
 
 while True:                             # The Event Loop
     event, values = window.read() 
-    print(event, values)       
+
+    regla_falsa(int(values['a_Param']),\
+                int(values['b_Param']),\
+                float(values['tol_Param']),\
+                int(values['n_Param']))
+
     if event == sg.WIN_CLOSED or event == 'Exit':
         break      
 
---
+++ regla_falsa.py
@@ -0,0 +1,65 @@
+import math
+import numpy as np
+
+def f(x):
+    f = math.pow(x,3) + 2*math.pow(x,2) - 3*x - 1
+    return f
+  
+# print("-- dada la funcion  math.pow(x,3) + 2*math.pow(x,2) - 3*x - 1")
+# print("-- y basado en el siguiente input de ejemplo")
+# print("-- Input de ejemplo: regla_falsa(1, 2, 0.0004, 100)")
+  
+def regla_falsa(a,b,TOL,N):
+    i = 1
+    FA = f(a)
+     
+    print("%-20s %-20s %-20s %-20s %-20s" % ("n","x_i","x_u","x_m","f(x_m)"))
+      
+    while(i <= N):
+        p = (a*f(b)-b*f(a))/(f(b) - f(a))
+        FP = f(p)
+          
+        if(FP == 0 or np.abs(f(p)) < TOL):
+            break
+        else:
+             print("%-20.8g %-20.8g %-20.8g %-20.8g %-20.8g\n" % (i, a, b, p, f(p)))
+         
+          
+        i = i + 1
+          
+        if(FA*FP > 0):
+            a = p
+        else:
+            b = p
+      
+    return
+
+
+# N = ''
+# TOL = ''
+# a = ''
+# b = ''
+
+
+# while not (a.strip("-").isnumeric()):
+# 	print('indique el punto inferior, parametro poscisional 1')
+# 	a = input()
+# a = int(a)
+
+
+# while not (b.strip("-").isnumeric()):
+# 	print('indique el punto superior, parametro poscisional 2')
+# 	b = input()
+# b = int(b)
+
+# print('indique la tolerancia, parametro poscisional 3')
+# TOL = input()
+# TOL = float(TOL)
+
+
+# while not (N.isnumeric()):
+# 	print('indique el numero maximo de iteraciones, parametro poscisional 4')
+# 	N = input()
+# N = int(N)
+
+#regla_falsa(a,b,TOL,N)
